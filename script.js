a1cd51a0cca6dc4b2f3dd59761b62df9ed1c2b18
rahman <hrepunzel@gmail.com>
2024-11-05 16:30:45 +0700

    update first-class citizen 2

--- script.js
+++ script.js
@@ -1,29 +1,27 @@
 // first-class cetizien start
 const calculate = document.getElementById("operator");
-calculate.onclick = (operation,numA,numB) => {
-    return operation(numA,numB);
+calculate.onclick = (operation,num1,num2) => {
+    return operation(num1,num2);
 }
-
-
-// operator aritmatika start
-const tambah = document.getElementById('tambah');
-tambah.onclick = () => {
-    return a + b;
-}
-const kali = document.getElementById('kali');
-kali.onclick = () => {
-    return a * b;
-}
-
-const kurang = document.getElementById('kurang');
-kurang.onclick = () => {
-    return a - b;
-}
-
-const bagi = document.getElementById('bagi');
-bagi.onclick = () => {
-    return a / b;
-}
+    // operator aritmatika start
+    const tambah = document.getElementById('tambah');
+    tambah.onclick = () => {
+        return operation(num1) + operation(num2);
+    }
+    const kali = document.getElementById('kali');
+    kali.onclick = () => {
+        return operation(num1) * operation(num2);
+    }
+    const kurang = document.getElementById('kurang');
+    kurang.onclick = () => {
+        return operation(num1) - operation(num2);
+    }
+    const bagi = document.getElementById('bagi');
+    bagi.onclick = () => {
+        return operation(num1) / operation(num2);
+    }
+    // operator aritmatika end
+// first-class cetizien end
 
 // const bagi = document.getElementById('bagi');
 // bagi.onclick = (a,b) => {
@@ -31,14 +29,17 @@ bagi.onclick = () => {
 //     this.b = b;
 // }
 
-// operator aritmatika end
 
 
         // function expression start
-            // const resultTambah = calculate(tambah, num1,num2);
-            // const resultKali   = calculate(kali, num1,num2);
-            // const resultKurang = calculate(kurang, num1,num2);
-            // const resultBagi   = calculate(bagi, num1,num2);
+            const resultTambah = calculate(tambah, num1,num2);
+            //resultTambah.innerHTML = num1 + num2; 
+            const resultKali   = calculate(kali, num1,num2);
+            //resultTambah.innerHTML = num1 * num2;
+            const resultKurang = calculate(kurang, num1,num2);
+            //resultTambah.innerHTML = num1 - num2;
+            const resultBagi   = calculate(bagi, num1,num2);
+            //resultBagi.innerHTML = num1 / num2;
         // function expression end
         
     const number   = document.getElementById("number");
@@ -52,7 +53,9 @@ bagi.onclick = () => {
     
 // result        
     const result = document.getElementById("result");
-        result.onclick = function() {
+    result.addEventListener("click",resultValue);
+
+    function resultValue() {
             const outputValue = document.getElementById("outputValue");
             const p = outputValue.getElementsByTagName("p");
             
@@ -65,8 +68,8 @@ bagi.onclick = () => {
     const clean = document.getElementById('clean');
         clean.addEventListener = ("click", () => {
             if(this.id == "erase") {
-        printHistory("");
-        printOutput("");
+                printHistory("");
+                printOutput("");
     }
 });
 // menghapus angka
