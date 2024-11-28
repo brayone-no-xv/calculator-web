0de1e381324abf16dbab20eb9da3a3fdd4423dfa
rahman <hrepunzel@gmail.com>
2024-11-28 19:32:01 +0700

    update all

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const AddOperatorFunction = document.querySelectorAll("#operator");
 const AddValueFunction = document.querySelectorAll("#number");
-const operator1 = parseInt(AddOperatorFunction[1]);
-const operator2 = parseInt(AddOperatorFunction[2]);
 
 AddValueFunction.forEach((button) => {
   button.onclick = (number) => {
@@ -16,49 +14,6 @@ AddOperatorFunction.forEach((button) => {
     document.getElementById("outputValue").value += value;
   };
 });
-
-// operator aritmatika start
-
-// if(AddOperatorFunction === '+'){
-//     numTambah.onclick = (operator1,operator2) => operator1 + operator2;
-//     };
-// } else if(AddOperatorFunction === '*') {
-//     numKali.onclick = (num1,num2) => {
-//     return num1 * num2;
-//     };
-// } else if(AddOperatorFunction === '-') {
-// numKurang.onclick = (num1,num2) => {
-//     return num1 - num2;
-// };
-// } else if(AddOperatorFunction === '/') {
-// numBagi.onclick = (num1,num2) => {
-//     return num1 / num2;
-// };
-// }
-
-// operator aritmatika end
-
-// function calculate(operation, num1, num2) {
-//     return operation(num1, num2); // false
-// };
-
-// first-class cetizien end
-
-// function expression start
-
-// const resultTambah = calculate(tambah,num1,num2);
-// document.getElementById("outputValue").value = eval(numTambah);
-
-// const resultKali   = calculate(kali,num1,num2);
-// document.getElementById("outputValue").value = eval(numKali);
-
-// const resultKurang = calculate(kurang,num1,num2);
-// document.getElementById("outputValue").value = eval(numKurang);
-
-// const resultBagi   = calculate(bagi, num1,num2);
-// document.getElementById("outputValue").value = eval(numBagi);
-
-// function expression end
 
 // menghapus angka start
 const clear = document.getElementById("clear");
