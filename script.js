92ef6ce9f10675537bab8ca8a1c10da8b1a8172d
rahman <hrepunzel@gmail.com>
2025-01-03 06:57:46 +0700

    update calculator

--- script.js
+++ script.js
@@ -1,39 +1,34 @@
 // mengambil elemen id
-const AddOperatorFunction = document.querySelectorAll("#operator");
-const AddValueButton = document.querySelectorAll("#number");
+const operatorButton = document.querySelectorAll("#operator");
+const numberButton = document.querySelectorAll("#number");
+const clearButton = document.getElementById("clear");
 const outputValue = document.getElementById("outputValue");
-const clearValue = document.getElementById("clear");
 
 // add number value start
-AddValueButton.forEach((button) => {
-  button.onclick = (number) => {
-    outputValue.value += number.target.value;
+numberButton.forEach((button) => {
+  button.onclick = () => {
+    outputValue.value += button.value;
   };
 });
 // add number value end
 
 // add operator function start
-AddOperatorFunction.forEach((button) => {
-  button.onclick = (operator) => {
-    outputValue.value += operator.target.value;
+operatorButton.forEach((button) => {
+  button.onclick = () => {
+    outputValue.value += button.value;
   };
 });
 // add operator function end
 
 // menghapus angka start
-clearValue.addEventListener("click", () => {
+clearButton.addEventListener("click", () => {
   outputValue.value = "";
 });
 // menghapus angka end
 
 // result start
-function ResultValue(id) {
-  const CurrentValue = outputValue.value;
-  const value = eval(CurrentValue);
-  setInterval(() => {
-    document.getElementById("outputValue").value = value;
-  }),
-    2000;
+function calculatorResult(id) {
+  outputValue.value = eval(outputValue.value);
 }
 
 // result end
